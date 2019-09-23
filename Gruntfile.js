2298a32f9a9f293910ef62e54d814b69280adf3a
alifianadexe <alifianadexe@gmail.com>
2019-09-23 02:46:13 +0000

    poasd

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
 var toml = require("toml");
 var S = require("string");
 
-var CONTENT_PATH_PREFIX = "../anbidev-blog/content";
+var CONTENT_PATH_PREFIX = "../anbidev-blog/content/posts";
 var SITE_IDX_DEST = "../anbidev-blog/static/js/pindex.json";
 
 module.exports = function(grunt) {
